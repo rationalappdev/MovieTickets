36d986397e6f2d46cebfcc0ffc87054f0d858006
Konstantin Shkut <Konstantin.Shkut@gmail.com>
2017-02-07 21:44:37 -0800

    updated redux reducer

--- src/redux.js
+++ src/redux.js
@@ -1,24 +1,5 @@
 const API = 'http://localhost:3000/v1';
 
-export const reducer = (state = { movies: [], loading: true }, action) => {
-  switch (action.type) {
-    case 'GET_MOVIE_DATA_LOADING':
-      return {
-        ...state,                   // keep the existing state,
-        loading: true,              // but change loading to true
-      };
-    case 'GET_MOVIE_DATA_RECEIVED':
-      return {
-        loading: false,             // set loading to false
-        movies: action.data.movies, // update movies array with reponse data
-      };
-    case 'GET_MOVIE_DATA_ERROR':
-      return state;
-    default:
-      return state;
-    }
-};
-
 export const apiMiddleware = store => next => action => {
   // Pass all actions through by default
   next(action);
@@ -43,4 +24,23 @@ export const apiMiddleware = store => next => action => {
     default:
       break;
   }
+};
+
+export const reducer = (state = { movies: [], loading: true }, action) => {
+  switch (action.type) {
+    case 'GET_MOVIE_DATA_LOADING':
+      return {
+        ...state,                   // keep the existing state,
+        loading: true,              // but change loading to true
+      };
+    case 'GET_MOVIE_DATA_RECEIVED':
+      return {
+        loading: false,             // set loading to false
+        movies: action.data.movies, // update movies array with reponse data
+      };
+    case 'GET_MOVIE_DATA_ERROR':
+      return state;
+    default:
+      return state;
+    }
 };
